c9d69d376c927946a5a59677fbba1ad7911f7be2
che <vlad.the.che@gmail.com>
2021-06-03 14:54:27 +0200

    Drawable, but wasted

--- src/main/java/sk/stuba/fei/uim/oop/ExamRT.java
+++ src/main/java/sk/stuba/fei/uim/oop/ExamRT.java
@@ -6,27 +6,32 @@ import sk.stuba.fei.uim.oop.Tree;
 
 public class ExamRT {
     public static void main(String[] args) {
-        Graphic graphic = new Graphic();
-        graphic.Paint();
+        JFrame f = new JFrame("Draw Box Mouse 2");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setContentPane(new Tree());
+        f.setSize(300, 300);
+        f.setVisible(true);
     }
-
-    public ExamRT() {
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
-                    ex.printStackTrace();
-                }
-
-                JFrame frame = new JFrame("Testing");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.add(new Tree());
-                frame.pack();
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
-            }
-        });
-    }
+//
+//    public ExamRT() {
+//        EventQueue.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+//                    ex.printStackTrace();
+//                }
+//
+//                JFrame frame = new JFrame("Testing");
+//                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//                Tree t = new Tree();
+//                frame.add(t);
+//                t.drawShape(frame.getGraphics());
+//                frame.pack();
+//                frame.setLocationRelativeTo(null);
+//                frame.setVisible(true);
+//            }
+//        });
+//    }
 }
--- src/main/java/sk/stuba/fei/uim/oop/Tree.java
+++ src/main/java/sk/stuba/fei/uim/oop/Tree.java
@@ -2,30 +2,58 @@ package sk.stuba.fei.uim.oop;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Tree extends JPanel {
+    private Color farba;
+    private int x;
+    private int y;
+    private int height;
+    private int width;
 
     public Tree() {
+
+        x = 0;
+        y = 200;
+        height = 100;
+        width = 100;
+        MyMouseListener listener = new MyMouseListener();
+        addMouseListener(listener);
+        addMouseMotionListener(listener);
     }
 
+    public void setStartPoint(int x, int y) {
+        this.x = x; this.y = y;
+    }
+
+    public void setEndPoint(int x, int y) {
+        width = Math.abs(this.x - x);
+        height = Math.abs(this.y - y);
+    }
+
+    class MyMouseListener extends MouseAdapter {
+        public void mousePressed(MouseEvent e) {
+            setStartPoint(e.getX(), e.getY());
+        }
+        public void mouseDragged(MouseEvent e) {
+            setEndPoint(e.getX(), e.getY()); repaint();
+        }
+        public void mouseReleased(MouseEvent e) {
+            setEndPoint(e.getX(), e.getY()); repaint();
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.fillRect(this.x+this.width/3,this.y+this.height/3,width/3,2*height/3);
+        g.fillOval(this.x+this.width/2,this.y+2*this.height/3,this.width,2*this.height/3);
+
+    }
     @Override
     public Dimension getPreferredSize() {
         return new Dimension(200, 200);
-    }
-
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g.create();
-        int width = getWidth();
-        int height = getHeight();
-
-        g2d.setColor(new Color(139, 69, 19));
-        g2d.fillRect((width / 2) - 20, height / 2, 40, height / 2);
-
-        g2d.setColor(Color.GREEN);
-        int radius = 60;
-        g2d.fillOval((width / 2) - radius, (height / 2) - (radius), radius * 2, radius * 2);
-        g2d.dispose();
     }
 
 }
