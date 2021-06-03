040a1c155b197ae7dd3e6aa2333f8d7940e35930
TetianaKukhelna <grey.shadow@ukr.net>
2021-06-03 16:22:59 +0200

    ---

--- src/main/java/sk/stuba/fei/uim/oop/ExamRT.java
+++ src/main/java/sk/stuba/fei/uim/oop/ExamRT.java
@@ -6,11 +6,6 @@ import sk.stuba.fei.uim.oop.Strom;
 
 public class ExamRT {
     public static void main(String[] args) {
-//        JFrame f = new JFrame("Draw Box Mouse 2");
-//        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        f.setContentPane(new Strom());
-//        f.setSize(300, 300);
-//        f.setVisible(true);
         Graphic graphic = new Graphic();
         graphic.Paint();
     }
